92c4ed78c78c28a24b966deb49b9d4a9c755374a
Filippo Mangione <filippomangione@outlook.com>
2023-12-29 18:14:35 +0100

    wip

--- cc-trova-la-paperella-unit/script.js
+++ cc-trova-la-paperella-unit/script.js
@@ -1,22 +1,28 @@
 const boxes = document.querySelectorAll('.box');
 const btnCheck = document.querySelector('.btn-check');
+const userInput = document.querySelector('.user-input');
 const message = document.querySelector('.message');
 
 const randomBoxIndex = Math.floor(Math.random() * 3) + 1;
 console.log(randomBoxIndex);
 
 function check() {
-  const selectedBoxIndex = 2;
-  const box = boxes[selectedBoxIndex - 1];
-  message.innerText = `Hai scelto la scatola: ${selectedBoxIndex}`;
-  console.log('check', randomBoxIndex);
+  const selectedBoxIndex = Number(userInput.value);
+  console.log(`Hai scelto la scatola: ${selectedBoxIndex}.`);
 
-  if (selectedBoxIndex === randomBoxIndex) {      
-    box.classList.add('duck');
-    message.innerText = 'Hai vinto :)';
+  if (!selectedBoxIndex) {
+    message.innerText = `Inserisci un numero da 1 a 3`;
+    return;
+  }
+
+  const box = boxes[selectedBoxIndex - 1];
+  
+  if (selectedBoxIndex === randomBoxIndex) {
+    box.classList.add('duck');    
+    message.innerText = `Hai vinto :)`;
   } else {
     box.classList.add('water');
-    message.innerText = 'Sbagliato!';
+    message.innerText = `Hai perso :(`;
   }
 }
 
--- cc-trova-la-paperella/script.js
+++ cc-trova-la-paperella/script.js
@@ -20,37 +20,3 @@ boxes.forEach(function(box, i) {
     isEndGame = true;
   });
 });
-
-/*
-const btnCheck = document.querySelector('.btn-check');
-const userInput = document.querySelector('.user-input');
-const boxes = document.querySelectorAll('.box');
-const message = document.querySelector('.message');
-let isEndGame = false;
-
-const randomBoxIndex = Math.floor(Math.random() * 3);
-console.log(randomBoxIndex);
-
-function check () {
-  const userValue = userInput.value;
-  console.log('check', userValue)
-
-  if (userValue === '') { 
-    message.innerText = 'Inserisci un numero da 1 a 3';
-    return;
-  } 
-  
-  if(userValue === randomBoxIndex) {
-    message.innerText = 'Hai vinto :)';
-  } else {
-    message.innerText = 'Hai perso :(';
-  }
-}
-
-
-btnCheck.addEventListener('click', check);
-*/
-
-
-
-
