a015c2ae757f2ade68870bdf5bb0197fb470941f
Filippo Mangione <filippomangione@outlook.com>
2023-12-30 21:11:46 +0100

    wip

---
+++ cc-paperella/cc-lancia-la-paperella/script.js
@@ -0,0 +1,5 @@
+const duck = document.querySelector('.duck');
+const boxes = document.querySelectorAll('.box');
+const message = document.querySelector('.message');
+
+// boxes[1].classList.add('duck');
